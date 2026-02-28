76b6f2877b57f8123a30bfd13b3ae9107d98f650
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-02-28 00:24:30 -0300

    TODO criado com funções previstas

--- profile_data.py
+++ profile_data.py
@@ -1,6 +1,26 @@
+# Script para gerar perfil de dados com validação de qualidade.
+
+import pandas as pandas
+from ydata_profiling import ProfileReport
+from pathlib import Path
+from datetime import datetime
+
+# TODO: achar uma rquivo csv para teste
+csv_file = ''
+
+max_duplicates_pct = 5.0 # %
+max_missing_pct = 10.0 # %
+
+# pasta para salvar os relatórios
+reports_dir = 'reports'
+
+# TODO:
+def carregar_dados():
+
+def gerar_relatorios():
+
+def extrair_estatisticas():
+
+def validar_qualidade():
+
 def main():
-    print("Hello from data-profiler-pipeline!")
-
-
-if __name__ == "__main__":
-    main()
